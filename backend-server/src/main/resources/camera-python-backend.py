ab39a506f0d4b9018cb9659dcc88e2b5bc6e8a5c
Lukasz Blasiak <lukasz.blasiak95@gmail.com>
2021-03-14 20:15:00 +0100

    Add bme280 python backend source code

--- backend-server/src/main/resources/camera-python-backend.py
+++ backend-server/src/main/resources/camera-python-backend.py
@@ -12,6 +12,7 @@ from requests_toolbelt import MultipartEncoder
 from flask import send_file, make_response
 from werkzeug import FileWrapper
 import sys
+import json
 
 
 
@@ -23,11 +24,17 @@ log.setLevel(logging.ERROR)
 camera = PiCamera()
 camera.resolution = (1280, 720)
 
+cracow_altitude = 257
+port = 1
+address = 0x76
+bus = smbus2.SMBus(port)
+calibration_params = bme280.load_calibration_params(bus, address)
+
 def OnExitApp():
     camera.close()
 atexit.register(OnExitApp)
 
-def capture_camera_preview_as_base64():
+def get_camera_preview_as_base64():
     image_stream = io.BytesIO()
     camera.capture(image_stream, 'jpeg', quality=30)
     image_stream.seek(0)
@@ -35,15 +42,35 @@ def capture_camera_preview_as_base64():
 
 
 @app.route('/cameraPreview')
-def getCameraPreviewAsBase64():
+def get_camera_preview_as_base64_response():
     key = request.args.get('key', default = '', type = str)
     if key == APP_KEY:
-        return capture_camera_preview_as_base64()
+        return get_camera_preview_as_base64()
+    else:
+        return "Incorrect APP KEY", 401
+
+def get_bme280_measurements():
+    data = bme280.sample(bus, address, calibration_params)
+    response_model = {}
+    response_model["temperature"] = data.temperature
+    response_model["humidity"] = data.humidity
+    response_model["pressure"] = data.pressure + cracow_altitude/8.3
+    return json.dumps(response_model)
+
+@app.route('/bme280')
+def get_bme280_measurements_response():
+    key = request.args.get('key', default = '', type = str)
+    if key == APP_KEY:
+        response_model = get_bme280_measurements()
+        return Response(response=response_model,
+                        status=200,
+                        mimetype="application/json")
     else:
         return "Incorrect APP KEY", 401
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0')
+
 
 
 
