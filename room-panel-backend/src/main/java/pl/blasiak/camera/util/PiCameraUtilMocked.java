f1122015a2ff17fb253d98f7158b9b3d15dc4bc6
lukasz <lukasz.blasiak@wp.pl>
2021-05-30 16:38:51 +0200

    Improve sending mocked camera preview

--- room-panel-backend/src/main/java/pl/blasiak/camera/util/PiCameraUtilMocked.java
+++ room-panel-backend/src/main/java/pl/blasiak/camera/util/PiCameraUtilMocked.java
@@ -1,15 +1,16 @@
 package pl.blasiak.camera.util;
 
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 import pl.blasiak.application.config.ProfilesConfig;
 import pl.blasiak.camera.exception.CameraException;
 import pl.blasiak.camera.mapper.ImageModelMapper;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 @Profile(ProfilesConfig.PROFILE_LOCAL)
@@ -17,22 +18,21 @@ import java.nio.charset.StandardCharsets;
 public class PiCameraUtilMocked extends PiCameraUtil {
 
     private static final String KITTEN_IMAGE_PATH = "classpath:mocked_preview_kitku.jpeg";
+    private final String encodedMockedPreviewImage;
 
-    public PiCameraUtilMocked(final ImageModelMapper imageModelMapper) {
+    public PiCameraUtilMocked(final ImageModelMapper imageModelMapper, @Value(KITTEN_IMAGE_PATH) final Resource resourceFile) {
         super(imageModelMapper);
+        try (final InputStream fileInputStream = resourceFile.getInputStream()) {
+            byte[] mockedPreviewImageBytes = fileInputStream.readAllBytes();
+            this.encodedMockedPreviewImage =
+                    new String(Base64.encodeBase64(mockedPreviewImageBytes), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new CameraException(e.getMessage(), e);
+        }
     }
 
     @Override
     public String getCameraImageAsBase64() {
-        try {
-            final var imgFile = ResourceUtils.getFile(KITTEN_IMAGE_PATH);
-            try(final FileInputStream fileInputStreamReader = new FileInputStream(imgFile)) {
-                byte[] bytes = new byte[(int)imgFile.length()];
-                fileInputStreamReader.read(bytes);
-                return new String(Base64.encodeBase64(bytes), StandardCharsets.UTF_8);
-            }
-        } catch (IOException e) {
-            throw new CameraException(e.getMessage(), e);
-        }
+        return this.encodedMockedPreviewImage;
     }
 }
